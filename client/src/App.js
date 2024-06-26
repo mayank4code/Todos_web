f2f7a6f6e96fe2270af657fe0d12d741d38f6fb3
Mayank Yadav <mayank4code@gmail.com>
2024-06-26 06:27:42 +0530

    first Commit of Simple ReactJs Todos

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import Todos_Item from "./Components/Todos_Item.js";
+
+function App() {
+  return(
+  <>
+    <div className="heading">
+      <h1> To_dos List </h1>
+    </div>
+    <Todos_Item/>
+  </>);
+}
+
+export default App;
